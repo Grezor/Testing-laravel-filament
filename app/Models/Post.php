29c42a394f21fc7a0ed282aa13fec599e27bb4ac
Geoffrey Duplessi <grezor.gamer@gmail.com>
2022-12-17 20:51:25 +0100

    create models and migrations

---
+++ app/Models/Post.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Tag;
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['category_id', 'title', 'slug', 'content', 'is_published'];
+
+    public function Category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+}
